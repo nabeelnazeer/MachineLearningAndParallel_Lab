b923b8b362a55556bde47ca7d89f9072b5609938
nabeelnazeer <nabeel777@ug.cusat.ac.in>
2025-03-27 13:25:24 +0530

    data

---
+++ Thread_n_Numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <thread> // Required for std::thread
+
+void printNaturalNumbers(int n) {
+    for (int i = 1; i <= n; ++i) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the value of n: ";
+    std::cin >> n;
+
+    // Create a thread to execute the print function
+    std::thread t(printNaturalNumbers, n);
+
+    // Wait for the thread to complete
+    t.join();
+
+    return 0;
+}
